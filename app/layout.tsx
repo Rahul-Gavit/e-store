0ae4973ac803a113626ba1ad2cdb491f93c608df
Rahul Gavit <rahul@easyhrworld.com>
2024-06-11 02:13:09 +0530

    Fixed issue API

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
+        <Providers session>
           <Navbar />
           {children}
           <Footer />
