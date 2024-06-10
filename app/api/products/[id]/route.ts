71f93dd66e64a68df42968d8ae53a8ab56c35e5e
Rahul Gavit <rahul@easyhrworld.com>
2024-06-11 02:05:00 +0530

    Fixed API issues

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -2,7 +2,7 @@ import connectMongoDB from "@/libs/mongodb";
 import Product from "@/models/product";
 import { NextResponse } from "next/server";
 
-export async function GET(request, { params }) {
+export async function GET(req: any, { params }: any) {
   try {
     const { id } = params;
 
@@ -15,7 +15,7 @@ export async function GET(request, { params }) {
     }
 
     return NextResponse.json({ product }, { status: 200 });
-  } catch (error) {
+  } catch (error: any) {
     console.error("Error fetching product:", error.message);
     return NextResponse.json(
       {
@@ -26,20 +26,20 @@ export async function GET(request, { params }) {
   }
 }
 
-export async function DELETE(req, { params }) {
+export async function DELETE(req: any, { params }: any) {
   try {
     const { id } = params;
 
     await connectMongoDB();
 
-    const product = await Product.findOneAndDelete({ productId: id });
+    const product = await Product.findOneAndDelete({ productI: id });
 
     if (!product) {
       throw new Error("Product not found");
     }
 
     return NextResponse.json({ product }, { status: 200 });
-  } catch (error) {
+  } catch (error: any) {
     console.error("Error deleting product:", error.message);
     return NextResponse.json(
       {
